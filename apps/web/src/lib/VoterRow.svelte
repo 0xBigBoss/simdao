312b41fa13c5f4c82d508c44a343deed8150bf98
BigBoss <0xbigboss@protonmail.com>
2022-07-07 22:28:13 -0500

    use correct avatar, format numbers

--- apps/web/src/lib/VoterRow.svelte
+++ apps/web/src/lib/VoterRow.svelte
@@ -2,28 +2,22 @@
 	import { onMount } from 'svelte';
 
 	import type { Voter } from './gql/snapshot';
-	import jazzicon from '@metamask/jazzicon';
 	import ExternalLinkIcon from 'heroicons/solid/external-link.svg';
 
 	export let voter: Voter;
 
 	let avatar = voter.avatar;
-	let avatar_default: string | null = null;
-
-	onMount(() => {
-		if (!avatar) {
-			avatar_default = jazzicon(100, parseInt(voter.address.slice(2, 10), 16)).innerHTML;
-		}
+	let formatter = new Intl.NumberFormat('en-US', {
+		style: 'decimal',
+		minimumFractionDigits: 0,
+		compactDisplay: 'short',
+		notation: 'compact'
 	});
 </script>
 
 <div class="avatar">
 	<div class="w-8 h-8 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-		{#if voter.avatar}
-			<img src={voter.avatar} alt={voter.name} />
-		{:else}
-			{@html avatar_default}
-		{/if}
+		<img src={`https://cdn.stamp.fyi/avatar/eth:${voter.address}?s=138`} alt={voter.name} />
 	</div>
 </div>
 <div class="group-hover:text-white w-3/4 text-ellipsis overflow-hidden ">
@@ -37,11 +31,11 @@
 </div>
 <div class="text-accent font-semibold flex justify-between gap-2 bg-inherit">
 	<div class="tooltip" data-tip="Voting power">
-		{voter.power}
+		{formatter.format(voter.power)}
 	</div>
 	<div class="tooltip tooltip-primary " data-tip="Influence over propsal">
 		<span class="text-primary">
-			{voter.influence}
+			{formatter.format(voter.influence)}
 		</span>
 	</div>
 </div>
